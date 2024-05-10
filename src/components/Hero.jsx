34f66009a3ace16b666af0edd9b6e9476205e606
abtaheetaseen <abtahee.siddiqui@northsouth.edu>
2024-05-10 15:16:07 +0600

    fifth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className='my-bg flex items-center justify-center'>
         <div className='text-center'>
-            <h1 className='text-2xl lg:text-3xl text-white font-bold'>YOUR BELLY KNOWS BEST</h1>
+            <h1 className='text-2xl md:text-3xl lg:text-4xl text-white font-bold tracking-widest'>We're Fresher! We're Tastier!</h1>
             <p className='text-gray-400 w-9/12 mx-auto my-5'>A restaurant is a place where you can sit down and relax, and let someone else do the cooking for you. </p>
             <Link to="/allFoods">
                 <button className='btn btn-sm text-black bg-[#ECBD00] border-2 hover:border-[#ECBD00] hover:text-white hover:bg-transparent'>All Food</button>
