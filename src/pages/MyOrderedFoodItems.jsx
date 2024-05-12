3534328826caf137e51c13e30a2c870c48fd78e5
abtaheetaseen <abtahee.siddiqui@northsouth.edu>
2024-05-12 14:18:10 +0600

    twenty-two commit

--- src/pages/MyOrderedFoodItems.jsx
+++ src/pages/MyOrderedFoodItems.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../provider/AuthProvider'
 import { RiDeleteBack2Fill } from "react-icons/ri";
+import Swal from 'sweetalert2';
 
 const MyOrderedFoodItems = () => {
 
@@ -18,6 +19,41 @@ const MyOrderedFoodItems = () => {
         })
     }, [])
 
+    const handleDelete = (id) => {
+        console.log(id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+
+            fetch(`http://localhost:3000/orders/${id}`, {
+            method: "DELETE"
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.deletedCount > 0){
+                   Swal.fire({
+                title: "Deleted!",
+                text: `Order has deleted.`,
+                icon: "success"
+              });
+            //   remove from ui
+            const remaining = myOrderedFoodItems.filter(item => item._id !== id);
+            setMyOrderedFoodItems(remaining);
+            }
+        })
+            }
+          });
+    }
+
+    // get date
     const showDate = new Date();
     const displayTodaysDate = showDate.getDate()+"/"+(showDate.getMonth()+1)+"/"+showDate.getFullYear();
 
@@ -52,7 +88,7 @@ const MyOrderedFoodItems = () => {
         <td>{displayTodaysDate}</td>
         <td>{food.buyerQuantity}</td>
         <td>
-            <button className='btn btn-sm bg-red-500 text-white rounded-full hover:bg-red-400'>
+            <button onClick={() => handleDelete(food._id)} className='btn btn-sm bg-red-500 text-white rounded-full hover:bg-red-400'>
             <RiDeleteBack2Fill />
             </button>
         </td>
