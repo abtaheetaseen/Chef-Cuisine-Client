f6d18b6c894ea3f026df3b1b90860582f97fd880
abtaheetaseen <abtahee.siddiqui@northsouth.edu>
2024-05-30 11:33:27 +0600

    final commit

--- src/components/BookTable.jsx
+++ src/components/BookTable.jsx
@@ -65,7 +65,7 @@ const BookTable = () => {
                                     </svg>
                                 </span>
 
-                                <input defaultValue={user.email} readOnly={true} type="email" name='email' className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address" required />
+                                <input defaultValue={user?.email} readOnly={true} type="email" name='email' className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address" required />
 
                             </div>
 
